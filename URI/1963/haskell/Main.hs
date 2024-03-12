63eb2a04f98e9c44dfdccde762fac7ab283b7d83
R-Stanz <stanz@tutamail.com>
2024-03-11 21:51:36 -0300

    Functional Programming 2nd Week Exercises (Haskell, After Fixes)

--- URI/1963/haskell/Main.hs
+++ URI/1963/haskell/Main.hs
@@ -1,16 +1,16 @@
 import Text.Printf
 
-get_values str pos = do
+format_values str pos = do
                     if (drop (pos-1) (take pos str) == " ")
                         then (take pos str, drop pos str)
                     else
-                        get_values str (pos+1)
+                        format_values str (pos+1)
 
 -- removePunc xs = [ x | x <- xs, not (x `elem` "\"") ]
 
 main = do
         str <- getLine
-        let (str_a, str_b) = get_values str 1
+        let (str_a, str_b) = format_values str 1
         let a = read str_a
         let b = read str_b
 	let result = 100*(b-a)/a
--- classes/functional_programming/2-week/ATIVIDADE02.hs
+++ classes/functional_programming/2-week/ATIVIDADE02.hs
@@ -1,40 +1,34 @@
 atividade = "2"
 
 -- 1 
--- Sejam as tuplas u e v de inteiros
--- tal que exista um inteiro k onde
--- u = kv ou v = ku  
--- então u e v são mútiplos. Construa 
--- função que determine se duas 
--- tuplas de inteiros  são múltiplas.
-isMult :: (Int) -> (Int) -> Bool
-isMult u v = if (((u `mod` v) == 0) || ((v `mod` u) == 0))
+isMult :: (Int, Int) -> (Int, Int) -> Bool
+isMult u v = if ((u `ini'test` v) || (v `ini'test` u))
                 then True
              else False
+             where ini'test x y = if ((fst x) `mod` (fst y) == 0)
+                                  then last'test (snd x) (snd y) ((fst x) `div` (fst y)) 
+                              else False
+                   last'test x y k = if (((x `mod` y) == 0) && ((x `div` y) == k))
+                                    then True
+                                else False
    
 -- 2
--- Sejam todos os triângulos retângulos
--- de perímetro p e de lados inteiros.
---   representados por tuplas (a,b,c) 
--- com  a>=b>=c. Criar  
---  função que determine 
--- o total destes triângulos dado p .
 tot'tri  :: Int -> Int
 tot'tri p = test p (1,1,1) 0
             where is'tri'rec (a,b,c) = if (b*b+c*c-a*a == 0)
                                            then True
-	                               else False
+                                       else False
                   test p (a,b,c) sum = if ((a+b+c == p) && (is'tri'rec (a,b,c)))
                                            then if (c == b)
-					       then if (c == a)
-					           then test p ((a+1),1,1) (sum+1)
-					       else test p (a,(b+1),1) (sum+1)
-					   else test p (a,b,(c+1)) (sum+1)
-				       else 
+                                               then if (c == a)
+                                                   then test p ((a+1),1,1) (sum+1)
+                                               else test p (a,(b+1),1) (sum+1)
+                                           else test p (a,b,(c+1)) (sum+1)
+                                       else 
                                           if (b+c > p)
                                               then sum 
                                           else if (c == b)
-					      then if (c == a)
-					          then test p ((a+1),1,1) sum
-					       else test p (a,(b+1),1) sum
-					  else test p (a,b,(c+1)) sum
+                                              then if (c == a)
+                                                  then test p ((a+1),1,1) sum
+                                              else test p (a,(b+1),1) sum
+                                          else test p (a,b,(c+1)) sum
