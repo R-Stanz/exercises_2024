c12953bff30eee79a6a9ad22cbe3801459a5622a
R-Stanz <stanz@tutamail.com>
2024-06-11 15:11:33 -0300

    Leetcode - 1051. 'Heights Checker' (Java)

---
+++ leetcode/height_checker/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+        public int heightChecker(int[] heights) {
+                
+                int[] sortedHeights = Arrays.copyOf(heights, heights.length);
+                Arrays.sort(sortedHeights);
+                
+                int count = 0;
+                for (int i = 0; i < heights.length; i++) {
+                        if (heights[i] != sortedHeights[i])
+                                count += 1;
+                }       
+                return count;
+        }
+} 
